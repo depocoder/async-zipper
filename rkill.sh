347c9f9c12a11ee2709bf278f23d911ae2c1c6e3
Ivan <60302361+depocoder@users.noreply.github.com>
2021-12-16 20:51:20 +0300

    Create rkill.sh

---
+++ rkill.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+### Current PID, if this script try to self search, then will loop forever
+pid=$$
+
+### Recursive function
+search()
+{
+    if [ $1 == $pid ]; then
+        break
+    fi
+
+    pgrep -P $1 |
+    while read process; do
+        echo $process
+        kill -9 $process
+        search $process
+    done
+}
+
+search $1
